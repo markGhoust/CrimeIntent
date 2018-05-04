c3c75702c4f1600d1493059cbb0deb505d39637a
fengping.ma <fengping.ma@tinno.com>
2018-05-04 18:14:19 +0800

    chapter thirteen at 2018:5:4--test

---
+++ app/src/main/java/database/CrimeDbSchema/CrimeDbSchema.java
@@ -0,0 +1,25 @@
+package database.CrimeDbSchema;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CrimeDbSchema {
+    public static final class CrimeTable{
+        public static final String NAME = "crimes";
+    }
+
+    public static final class Cols{
+        static {
+            List<String> lists = new ArrayList<>();
+            for(String string: lists) {
+
+            }
+            for (int i = 0; i < 100; i++) {
+
+            }
+            for (String list : lists) {
+
+            }
+        }
+    }
+}
